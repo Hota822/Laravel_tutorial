ae02fad7b125280bc4290a8cb657602569f56b6a
hota822 <h_takayama@ziouz.co.jp>
2019-11-11 09:08:43 +0000

    start jenkins

--- resources/views/users/user.blade.php
+++ resources/views/users/user.blade.php
@@ -2,7 +2,7 @@
     <li>
 	{!! HTML::decode(link_to(url('users/'.$user->id),  Helper::gravatar_for($user, 50))) !!}
 	{{ link_to("users/{$user->id}", $user->name) }}
-	@if ($admin->admin && $user != $admin)
+	@if ($admin->admin && $user->id != $admin->id)
 	    |
 	    @include('shared.delete_link', ['to' => 'user', 'to_id' => $user->id])
 	@endif 
